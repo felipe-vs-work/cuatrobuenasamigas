e6cfb71902d351de7044abe98b47332d3bad055c
MRC Macbook <mrcmacbook@MRCs-MacBook-Pro.local>
2018-12-04 16:34:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 	<body>
 		<h1>Hola, estoy probando GitHub</h1>
 		<p>Para ser más organizado.</p>
+		
+		<h2>Este título lo hice desde Github</h2>
+		<p>Qué entretenido, <strong>este texto lo puse desde mi editor en un pull request, como corrección esperando una respuesta.</strong></p>
 	</body>
 </html>
